667afc308db7b31ff171965e8d04518521dd002e
JanMunster <janmunster@gmail.com>
2021-02-27 16:35:12 +0100

    Videos added to game screen. Working on controlling videos

--- BattleshipWPF/GameWindow.xaml.cs
+++ BattleshipWPF/GameWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,13 +20,70 @@ namespace BattleshipWPF
     /// </summary>
     public partial class GameWindow : Window
     {
-        public PlayerModel Human{ get; set; }
+        public PlayerModel Human { get; set; }
         public PlayerModel Computer { get; set; }
         public GameWindow(PlayerModel hum, PlayerModel com)
         {
             Human = hum;
             Computer = com;
             InitializeComponent();
+            DrawGrid();
+            StartVideos();
+        }
+
+        private void StartVideos()
+        {
+            navyfootage.Play();
+            navyfootage2.Play();
+        }
+
+        private void DrawGrid()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ColumnDefinition gridCol = new ColumnDefinition();
+                gridCol.Name = "Column" + i.ToString();
+                GameGrid.ColumnDefinitions.Add(gridCol);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                RowDefinition gridRow = new RowDefinition();
+                gridRow.Name = "Row" + i.ToString();
+                GameGrid.RowDefinitions.Add(gridRow);
+            }
+
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    Button button = new Button();
+                    button.VerticalAlignment = VerticalAlignment.Stretch;
+                    button.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    button.Tag = (x, y);
+                    button.FontSize = 32;
+                    button.AddHandler(Button.ClickEvent, new RoutedEventHandler(GameClick));
+                    Grid.SetColumn(button, x);
+                    Grid.SetRow(button, y);
+                    GameGrid.Children.Add(button);
+                }
+            }
+        }
+
+        private void GameClick(object sender, RoutedEventArgs e)
+        {
+            navyfootage.Pause();
+            navyfootage.Visibility = Visibility.Hidden;
+            humanfiringvideo.Visibility = Visibility.Visible;
+            humanfiringvideo.Play();
+            humanfiringvideo.MediaEnded += Humanfire_MediaEnded;
+        }
+
+        private void Humanfire_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            humanfiringvideo.Visibility = Visibility.Collapsed;
+            navyfootage.Visibility = Visibility.Visible;
+            navyfootage.Play();
         }
     }
 }
